d30c179926780da505e9ee2155431c168993fd59
Khaled Issa <khaled.ielmt@hotmail.com>
2019-11-05 18:45:14 +0200

    served an HTML file

--- myApp.js
+++ myApp.js
@@ -11,15 +11,11 @@ var app = express();
 console.log("Hello world");
 
 
-/** 2) A first working Express Server */
-app.get("/", function(req, res) {
-    res.send("Hello Express");
-    });
 
-
-
-/** 3) Serve an HTML file */
-
+app.get("/", function(req, res){
+    res.sendfile(__dirname + '/views/index.html');
+  });
+  
 
 /** 4) Serve static assets  */
 
